b71317f2aec52d4e2b898c273412039454603913
Alberto Asuero <alberto.asuero@geographica.gs>
2015-11-17 00:02:12 +0100

    PostgreSQL-9.1.2-PostGIS-1.5.8

--- PostgreSQL-9.1.2-PostGIS-1.5.8-Patched/README.md
+++ PostgreSQL-9.1.2-PostGIS-1.5.8-Patched/README.md
@@ -1,21 +1,19 @@
-PostgreSQL 9.3.5, PostGIS 2.1.7, GDAL 1.11.2, Patched
+PostgreSQL 9.1.2, PostGIS 1.5.8 Patched
 =====================================================
 
-This is the README.md for Docker tag __postgresql-9.3.5-postgis-2.1.7-gdal-1.11.2-patched__.
+This is the README.md for Docker tag __postgresql-9.1.2-postgis-1.5.8-patched__.
 
 Versions
 --------
 This Dockerfile compiles the following software:
 
-- __PostgreSQL 9.3.5;__
+- __PostgreSQL 9.1.2;__
 
 - __GEOS 3.4.2;__
 
-- __Proj 4.9.1:__ patched with the spanish national grid for conversion between ED50 to ETRS89;
+- __Proj 4.8.0:__ patched with the spanish national grid for conversion between ED50 to ETRS89;
 
-- __GDAL 1.11.2:__ also patched;
-
-- __Postgis 2.1.7:__ patched as well.
+- __Postgis 1.5.8:__ patched as well.
 
 Usage Pattern
 -------------
@@ -24,13 +22,13 @@ Build the image directly from Git (this can take a while, don't forget to checko
 ```Shell
 cd gitfolder
 
-docker build -t="geographica/postgis:postgresql-9.3.5-postgis-2.1.7-gdal-1.11.2-patched" .
+docker build -t="geographica/postgis:postgresql-9.1.2-postgis-1.5.8-patched" .
 ```
 
 or pull it from Docker Hub:
 
 ```Shell
-docker pull geographica/postgis:postgresql-9.3.5-postgis-2.1.7-gdal-1.11.2-patched
+docker pull geographica/postgis:postgresql-9.1.2-postgis-1.5.8-patched
 ```
 
 The image uses several environment variables. Refer to the Dockerfile for a complete list. The most important one is __POSTGRES_PASSWD__, the password for the user POSTGRES.
@@ -38,7 +36,7 @@ The image uses several environment variables. Refer to the Dockerfile for a comp
 The image exposes port 5432 and a volume designated by enviroment variable __POSTGRES_DATA_FOLDER__. In a production enviroment, create containers this way:
 
 ```Shell
-docker run -d -P --name whatever -e "POSTGRES_PASSWD="md5"$(printf '%s' "change_this_password" "postgres" | md5sum | cut -d ' ' -f 1)" geographica/postgis:postgresql-9.3.5-postgis-2.1.7-gdal-1.11.2-patched 
+docker run -d -P --name whatever -e "POSTGRES_PASSWD="md5"$(printf '%s' "change_this_password" "postgres" | md5sum | cut -d ' ' -f 1)" geographica/postgis:postgresql-9.1.2-postgis-1.5.8-patched 
 ```
 
 This generates a MD5 hashed password for the user __postgres__. Keep in mind that to provide a MD5-hashed password to PostgreSQL it has to be the hash of __passwordusername__ and be prefixed by __md5__.
---
+++ PostgreSQL-9.1.2-PostGIS-1.5.8-Patched/packages/compile.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+while [[ $# > 1 ]]
+do
+key="$1"
+case $key in
+  -e|--encoding)
+  ENCODING="$2"
+  shift # past argument
+  ;;
+  -l|--locale)
+  LOCALE="$2"
+  shift # past argument
+  ;;
+  --pg-version)
+  PG_VERSION="$2"
+  shift # past argument
+  ;;
+  --geos-version)
+  GEOS_VERSION="$2"
+  shift # past argument
+  ;;
+  --proj4-version)
+  PROJ4_VERSION="$2"
+  shift # past argument
+  ;;
+
+   --postgis-version)
+  POSTGIS_VERSION="$2"
+  shift # past argument
+  ;;
+
+  *)
+          # unknown option
+  ;;
+esac
+shift # past argument or value
+done
+
+# Update and apt-get basic packages
+
+apt-get update && apt-get install -y build-essential gcc-4.7 python python-dev libreadline6-dev zlib1g-dev libssl-dev libxml2-dev libxslt-dev locales
+
+localedef -i $LOCALE -c -f $ENCODING -A /usr/share/locale/locale.alias ${LOCALE}.${ENCODING}
+
+locale-gen ${LOCALE}.${ENCODING}
+
+# Untar
+cd src ; tar -xjvf postgresql-${PG_VERSION}.tar.bz2 ; cd ..
+
+cd src ; tar -xjvf geos-${GEOS_VERSION}.tar.bz2 ; cd ..
+
+cd src ; tar -xvf proj-${PROJ4_VERSION}.tar.gz ; cd ..
+
+cd src ; mkdir -p proj-datumgrid ; cd ..
+
+cd src ; tar -xvf proj-datumgrid-1.5.tar.gz -C proj-datumgrid ; cd ..
+
+cd src ; tar -xvf postgis-${POSTGIS_VERSION}.tar.gz ; cd ..
+
+
+# Compilation of PostgreSQL
+cd src/postgresql-${PG_VERSION} ; ./configure CC='gcc-4.7 -m64' --prefix=/usr/local --with-pgport=5432 --with-python --with-openssl --with-libxml --with-libxslt --with-zlib ; cd ../..
+
+cd src/postgresql-${PG_VERSION} ; make ; cd ../..
+
+cd src/postgresql-${PG_VERSION} ; make install ; cd ../..
+
+cd src/postgresql-${PG_VERSION}/contrib ; make all ; cd ../../..
+
+cd src/postgresql-${PG_VERSION}/contrib ; make install ; cd ../../..
+
+groupadd postgres
+
+useradd -r postgres -g postgres
+
+ldconfig
+
+# Compilation of GEOS
+cd src/geos-${GEOS_VERSION} ; ./configure ; cd ../..
+
+cd src/geos-${GEOS_VERSION} ; make ; cd ../..
+
+cd src/geos-${GEOS_VERSION} ; make install ; cd ../..
+
+ldconfig
+
+# Compilation of Proj 4
+mv src/proj-datumgrid/* src/proj-${PROJ4_VERSION}/nad
+
+mv src/pj_datums.c src/proj-${PROJ4_VERSION}/src
+
+mv src/epsg src/proj-${PROJ4_VERSION}/nad/
+
+mv src/PENR2009.gsb src/proj-${PROJ4_VERSION}/nad/
+
+# chown -R 142957:5000 src/proj-${PROJ4_VERSION}
+
+cd src/proj-${PROJ4_VERSION} ; ./configure ; cd ../..
+
+cd src/proj-${PROJ4_VERSION} ; make ; cd ../..
+
+cd src/proj-${PROJ4_VERSION} ; make install ; cd ../..
+
+ldconfig
+
+
+# Compilation of PostGIS
+mv src/spatial_ref_sys.sql src/postgis-${POSTGIS_VERSION}/
+
+cd src/postgis-${POSTGIS_VERSION} ; ./configure ; cd ../..
+
+cd src/postgis-${POSTGIS_VERSION} ; make ; cd ../..
+
+cd src/postgis-${POSTGIS_VERSION} ; make install ; cd ../..
+
+ldconfig
+
+# Clean up
+apt-get clean && rm -rf /var/lib/apt/lists/* && rm -Rf /usr/local/src && apt-get remove -y --purge build-essential gcc-4.7 python python-dev libreadline6-dev zlib1g-dev libssl-dev libxml2-dev libxslt-dev 
+
+chmod 755 /usr/local/bin/run.sh
+
+chown postgres:postgres /usr/local/bin/run.sh
