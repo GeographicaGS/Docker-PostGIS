1af9d1be604e7a1edeffc4e1c1398d85fce3537e
cayetanobv <cayetano.benavent@geographica.gs>
2016-12-01 11:10:39 +0100

    removed deprecated tag: PostgreSQL-9.5.0-PostGIS-2.2.1-GDAL-2.0.2-Patched

--- PostgreSQL-9.5.0-PostGIS-2.2.1-GDAL-2.0.2-Patched/Usage_Cases/Assets/Create_role.sql
+++
@@ -1,3 +0,0 @@
-\echo Creating project role
-
-create role project with login inherit password 'project_pass';
--- PostgreSQL-9.5.0-PostGIS-2.2.1-GDAL-2.0.2-Patched/Usage_Cases/Assets/Schema02_DDL.sql
+++
@@ -1,5 +0,0 @@
-\echo Create test_schema schema
-
-\c project2 project2
-
-create schema test_schema;
